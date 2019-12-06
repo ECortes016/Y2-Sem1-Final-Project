2aa9d5bac6a96e396b600a7fb214a4212105a6f4
Emmanuel Cortes <ecortes016@west-mec.org>
2019-12-06 15:14:30 -0700

    Added EJS Layouts
    Added an EJS Layout for the coin pages

--- app.js
+++ app.js
@@ -2,28 +2,32 @@ var express = require('express');
 var app = express();
 var fetch = require('node-fetch');
 var path = require('path');
+// npm install --save express-ejs-layouts
+var expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts)
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.get('/:coin_id', async(req, res) => {
+
+app.get('/coins/:coin_id', async (req, res) => {
     try {
         coinData = await fetch(`https://api.coinranking.com/v1/public/coin/${req.params.coin_id}`);
 
         const json = await coinData.json();
         console.log(json);
 
-        const {...data} = json.data;
+        const { ...data } = json.data;
         // console.log("coin", data);
         res.render('index', {
-            data: {
-                name: json.data.coin.name,
-                description: json.data.coin.description,
-                image: json.data.coin.iconUrl,
-                price: json.data.coin.price, 
-                rank: json.data.coin.rank,
-                sign: json.data.base.sign,
-                symbol: json.data.base.symbol,
-            }
+            name: json.data.coin.name,
+            description: json.data.coin.description,
+            image: json.data.coin.iconUrl,
+            price: json.data.coin.price,
+            rank: json.data.coin.rank,
+            sign: json.data.base.sign,
+            symbol: json.data.base.symbol,
+            socials: json.data.coin.socials
         })
     } catch (error) {
         console.log(error)
